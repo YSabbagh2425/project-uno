2e2b3f5846f59e3ef75f50ba5b029514b891b29a
ysabbagh2425 <yousif.sabbagh@yahoo.com>
2023-07-14 09:43:56 +1000

    Update to HTML, CSS and JS

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project 1 - 777 Jackpot
+A game project that includes a slot machine based around the 777 Jackpot but with numbers in a 3x3 grid, done roughly in a week. The game is essentially gambling on money where you place an amount to bet to which you assume it will hit a pattern. If it does not hit what you bet on then you lose money and it becomes an endless cycle of addiction to reach the state of a winner. Enjoy miserably aiming to win virtual money.
+
+![slot-machine]
--- js/main.js
+++ js/main.js
@@ -1,14 +1,93 @@
-const variables = [1, 3, 5, 7];
+let variables = [1, 3, 5, 7];
+
+let betAmount = null;
+
+let newValue;
+
+let currentAmount = 100;
 
 const twoTimes = variables[0];
 
 const fiveTimes = variables[1];
 
-const tenTimes = variables [2];
+const tenTimes = variables[2];
 
-const fiftyTimes = variables [3];
+const fiftyTimes = variables[3];
 
-const addOne = document.querySelector[".one"];
+document.querySelector(".button").addEventListener('click', prayToRNG);
+document.querySelector(".one").addEventListener('click', addOne);
+document.querySelector(".all").addEventListener('click', addAll);
 
-const addAll = document.querySelector[".all"];
+init();
 
+function init () {
+    betAmount = document.querySelector(".bet")
+    currentAmount = document.querySelector(".income");
+};
+
+function randomNumberGenerator () {
+    let random = [];
+
+    for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * 3);
+        random.push(variables[Math.floor(Math.random() * 3)])
+    }
+    return random;
+};
+
+class Random {
+    constructor(random) {
+        this.random = random.reverse();
+    }
+    print() {
+        return this.random.join('');
+    }
+};
+
+const random1 = new Random(randomNumberGenerator());
+const random2 = new Random(randomNumberGenerator());
+const random3 = new Random(randomNumberGenerator());
+
+
+
+function  addOne () {
+    newValue = document.querySelector[".one"] 
+    newValue -= newValue.currentAmount
+} 
+
+function addAll () {
+    newValue = document.querySelector[".all"];
+    newValue -= allIn.currentAmount;
+};
+
+function prayToRNG () {
+    if(currentAmount > 0){
+    currentAmount -= betAmount
+    console.log(`Now, you have amount $${currentAmount}`)
+    randomNumberGenerator()
+    }
+};
+
+function checkPattern(){
+  
+    if ( twoTimes === 3 ) {
+      let amountWon = amountBet * 2; 
+      amountBet = amountWon;  
+      currentAmount.innerHTML = amountWon;
+  
+    }else if ( fiveTimes === 3 ) {
+        let amountWon = amountBet * 5; 
+        amountBet = amountWon;  
+        currentAmount.innerHTML = amountWon;
+    }else if ( tenTimes === 3 ) {
+        let amountWon = amountBet * 10; 
+        amountBet = amountWon;  
+        currentAmount.innerHTML = amountWon;
+    }else if ( fiftyTimes === 3 ) {
+        let amountWon = amountBet * 50; 
+        amountBet = amountWon;  
+        currentAmount.innerHTML = amountWon;
+    } else {
+      console.log("Lost money");
+    }
+  }

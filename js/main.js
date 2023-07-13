b1ad24bdde162b47e5af129c07c62c00cd14b4eb
ysabbagh2425 <yousif.sabbagh@yahoo.com>
2023-07-13 23:43:29 +1000

    Finished up touching HTML and CSS now finishing games logic

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const variables = [1, 3, 5, 7];
+
+const twoTimes = variables[0];
+
+const fiveTimes = variables[1];
+
+const tenTimes = variables [2];
+
+const fiftyTimes = variables [3];
+
+const addOne = document.querySelector[".one"];
+
+const addAll = document.querySelector[".all"];
+
